227151569180276f71b83420ed820da1518a9289
Samuel <mizzisamuel@gmail.com>
2015-05-09 17:07:22 +0200

    edge getters & setters

--- src/main/java/mt/edu/um/Edge.java
+++ src/main/java/mt/edu/um/Edge.java
@@ -3,13 +3,47 @@ package mt.edu.um;
 public class Edge 
 {
 	private Vertex source, destination;   
-	private double weight;
+	private double distance;
 	
 	
 	public Edge ()  { }
 	
-	public Edge (Vertex src, Vertex dst, double w)
+	public Edge (Vertex src, Vertex dst, double d)
 	{
-		
+		this.setSource(src);
+		this.setDestination(dst);
+		this.setDistance(d);
+	}
+	
+	
+	public void setSource (Vertex src)
+	{
+		source = src;
+	}
+	
+	public void setDestination (Vertex dst)
+	{
+		destination = dst;
+	}
+	
+	public void setDistance (double d)
+	{
+		distance = d;
+	}
+	
+	
+	Vertex getSource ()
+	{
+		return source;
+	}
+	
+	Vertex getDestination ()
+	{
+		return destination;
+	}
+	
+	double getDistance ()
+	{
+		return distance;
 	}
 }
