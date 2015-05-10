2929dce7767099b0fe57ceb7865b3343ec757004
Samuel <mizzisamuel@gmail.com>
2015-05-10 09:51:56 +0200

    Revert "revert"
    This reverts commit 4e856b27852e506a31924dd6b3af21be1f64cf70.

--- src/main/java/mt/edu/um/Main.java
+++ src/main/java/mt/edu/um/Main.java
@@ -9,13 +9,9 @@ public class Main
         
         DiGraph dg = new DiGraph();
         
-<<<<<<< HEAD
         dg.buildGraph(4, 0.5);
         
         System.out.println(dg.getVertices());
         System.out.println(dg.getEdges());
-=======
-        dg.buildGraph(4, 0.77);
->>>>>>> parent of a50cb74... more updates
     }
 }
