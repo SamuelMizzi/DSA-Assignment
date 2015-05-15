add3f2357bdec3456d4d44ec5f027609e0fc80ee
Samuel <mizzisamuel@gmail.com>
2015-05-15 22:13:57 +0200

    dijkstra

--- src/main/java/mt/edu/um/ShortestPath.java
+++ src/main/java/mt/edu/um/ShortestPath.java
@@ -2,8 +2,13 @@ package mt.edu.um;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 
 public class ShortestPath  // finding the shortest path between 2 vertices (q & r), using Dijkstra's Algorithm
@@ -28,8 +33,58 @@ public class ShortestPath  // finding the shortest path between 2 vertices (q & 
 		}
 		return 0;
     
-	}
 	
+	Set<Vertex> processed = new HashSet<Vertex>();
+    Map<Vertex, Integer> cost = new TreeMap<Vertex, Integer>();
+    Map<Vertex, List<Vertex>> pre = new HashMap<Vertex, List<Vertex>>();
+    //Sets initial costs to infinity for all vertices except sourceVertex
+    for (Vertex v: vertices) {
+            pre.put(v, new ArrayList<Vertex>());
+            if (v.equals(start)) {
+                    cost.put(start, 0);
+            }
+            else {
+                    cost.put(v, Integer.MAX_VALUE);
+            }
+    }
+    while (vertices.size() != processed.size()) {
+            Vertex min = null;
+            Integer minimum = Integer.MAX_VALUE;
+            for (Vertex v: cost.keySet()) {
+                    if (cost.get(v).compareTo(minimum) <= 0
+                                    && !processed.contains(v)) {
+                            minimum = cost.get(v);
+                            min = v;
+                    }
+            }
+            processed.add(min);
+            for (Vertex neighbor: getNeighbours(min)) {
+                    if (!processed.contains(neighbor)) {
+                            if (cost.get(neighbor).compareTo(cost.get(min)
+                                            + getEdge(min, neighbor)) > 0) {
+                                    Integer newVal = cost.get(min)
+                                                    + getEdge(min, neighbor);
+                                    cost.remove(neighbor);
+                                    cost.put(neighbor, newVal);
+                                    ArrayList<Vertex> mod = new ArrayList<Vertex>(pre.get(min));
+                                    mod.add(neighbor);
+                                    pre.put(min, mod);
+                            }
+                    }
+            }
+    }
+    if (pre.get(target).size() == 0) {
+            return -1;
+    }
+    ArrayList<Vertex> check = new ArrayList<Vertex>(pre.get(target));
+    while (check.get(0).compareTo(start) != 0) {
+            Vertex temp = check.get(0);
+            check.addAll(0, pre.get(temp));
+    }
+    check.add(target);
+    shortestPath.addAll(check);
+    return cost.get(target);
+}
 	
 	
 	public double getDistance(Vertex start, Vertex target) 
@@ -51,4 +106,17 @@ public class ShortestPath  // finding the shortest path between 2 vertices (q & 
 	      return d;
 	    }
 	  }  */
+	public List<Vertex> getNeighbours (Vertex v)
+	{
+		List<Vertex> neighbours = new ArrayList<Vertex>();
+		for (Edge E : edges)
+		{
+			if (E.getSource().equals(v))
+			{
+				neighbours.add(E.getDestination());
+			}
+		}
+		
+		return neighbours;			
+	}
 }
